38113b5f62fa2187b2c91c431f500c32abd02379
AGMadera <ponchp.agm@gmail.com>
2017-05-19 23:30:29 -0500

    Se agrega patron de diseño DAO

---
+++ Model/DAO/Imp/DAOUsuarioImp.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alfonsogalvanmadera
+ * Date: 19/05/17
+ * Time: 23:23
+ */
+class DAOUsuarioImp implements DAOUsuario
+{
+
+    public function login(Trabajador $trabajador)
+    {
+        // TODO: Implement login() method.
+
+    }
+
+    public function registroTrabajadores(Usuario $usuario)
+    {
+        // TODO: Implement registroTrabajadores() method.
+    }
+
+    public function validarVoto(Voto $voto)
+    {
+        // TODO: Implement validarVoto() method.
+    }
+}
