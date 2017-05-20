2153ee5334503e5462fdc293346ca49b47f7b9bc
AGMadera <ponchp.agm@gmail.com>
2017-05-20 00:06:55 -0500

    Se agrega funcionalidad login

--- Model/DAO/Imp/DAOUsuarioImp.php
+++ Model/DAO/Imp/DAOUsuarioImp.php
@@ -8,10 +8,27 @@
  */
 class DAOUsuarioImp implements DAOUsuario
 {
+    private $conexion;
+
+    public function __construct()
+    {
+        require_once('../confing/Conexion.php');
+        $this->conexion= new conexion();
+        $this->conexion->conectar();
+    }
 
     public function login( Usuario $usuario)
     {
         // TODO: Implement login() method.
+        $sql="SELECT * FROM Usuario WHERE user='$usuario->getUser()' AND pass='$usuario->getPass()'";
+        $result=$this->conexion->conexion->query($sql);
+        if ($result->num_rows > 0) {
+            $r=$result->fetch_array(MYSQLI_ASSOC);
+        }else{
+            $r= 0; //"no hay usuario";
+        }
+        $this->conexion->cerrar();
+        return $r;
 
     }
 
